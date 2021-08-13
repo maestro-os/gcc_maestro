858bbeb48f11b7e5236b4ea3d6ba47b92464a092
llenotre <llenotre@student.42.fr>
2021-08-13 21:44:01 +0200

    Added config.bfd patch

--- patch.sh
+++ patch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sed -i 's/^	gnu\* |/	gnu* | maestro* |/' binutils/config.sub
+sed -i '/^  i\[3-7\]86-\*-solaris2\*)/i \ \ i[3-7]86-*-maestro*)\n    targ_defvec=i386_elf32_vec\n    targ_selvecs=\n    targ64_selvecs=x86_64_elf64_vec\n    ;;' binutils/bfd/config.bfd
