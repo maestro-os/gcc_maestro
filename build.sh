53b86f019049c9fe0c019cc0783f4baaae2faf4e
llenotre <llenotre@student.42.fr>
2021-11-01 01:19:09 +0100

    WIP

--- build.sh
+++ build.sh
@@ -9,18 +9,18 @@ mkdir -p $SYSROOT/usr/include/
 # Building binutils
 mkdir binutils-build
 cd binutils-build
-../binutils/configure --target="$TARGET" --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --disable-werror
+../binutils/configure --target="$TARGET" --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --disable-nls --disable-werror
 make
-make install
+make install -j1
 cd ..
 
 # Building gcc
 mkdir gcc-build
 cd gcc-build
-../gcc/configure --target="$TARGET" --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --enable-languages=c,c++ --enable-shared
+../gcc/configure --target="$TARGET" --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --enable-languages=c,c++ --with-newlib --without-headers --disable-shared --disable-nls --disable-multilib
 make all-gcc
-make all-target-libgcc
 make install-gcc
+make all-target-libgcc
 make install-target-libgcc
 cd ..
 
@@ -29,7 +29,6 @@ cd musl
 export PATH="$PATH:$SYSROOT/usr/bin"
 ./configure --target="$TARGET" --prefix="$SYSROOT/usr" --syslibdir="$SYSROOT/lib"
 cp -r include/* $SYSROOT/usr/include/
-cp -r arch/i386/* $SYSROOT/usr/include/ # TODO Adapt to other arch
 make
 make install
 cd ..
